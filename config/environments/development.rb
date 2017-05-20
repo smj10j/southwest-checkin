97a908d3abbae1f3f2a589704668b8537c68578b
Stephen Johnson <stevej@ucla.edu>
2017-05-20 14:10:01 -0700

    Hybird dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,14 +4,15 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
 
   config.action_mailer.default_url_options = {
     host: 'localhost',
@@ -27,7 +28,7 @@ Rails.application.configure do
   config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
 
   # So assets have the full url
-  config.action_mailer.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = 'https://southwest.mac.smj10j.net'
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -41,7 +42,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
--- start-check-me-in.sh
+++ start-check-me-in.sh
@@ -4,37 +4,51 @@
 set -e
 
 # Debug
-# set -x
+set -x
 
 CWD=$(pwd)
 cleanup() {
+    log "Failed with error code $3 at $1:$2"
     cd "${CWD}"
-    
-    echo "Done!"
+    exit 1
 }
-trap 'cleanup' SIGTERM ERR
+trap 'cleanup "${BASH_SOURCE#*\./}" ${LINENO} $?' SIGTERM ERR
 
-
-
+log() {
+    echo "$(date +'%x %X') $1"
+}
 
 cd "/Code/smj10j/southwest-checkin"
 
+export RBENV_ROOT=/usr/local/var/rbenv
+eval "$(rbenv init -)"
+
 # echo "Launching redis..."
 # redis-server /usr/local/etc/redis.conf &
-# 
+#
 # echo "Launching postgres..."
 # pg_ctl -D /usr/local/var/postgres-9.6.1 -l postgres-log.log start &
 
 
-echo "Launching sidekiq..."
+## ENVIRONMENT and SECRET_KEY_BASE are set in launchctl plist net.smj10j.mac.southwest-checkin
+
+echo "Installing any needed bundles..."
+bundle install
+
+log "Launching sidekiq..."
 bundle exec sidekiq 2>&1 &
 
-echo "Launching webserver..."
-rails s 2>&1 & 
+log "Launching webserver..."
+bundle exec rails s -e "${ENVIRONMENT}" 2>&1 &
 
 wait %1 %2
 
 # Keep-alive# : update existing `sudo` time stamp until finished
-# while read -n1; do 
+# while read -n1; do
 #     sleep 1
 # done 2>/dev/null
+
+
+
+cd "${CWD}"
+log "Exited cleanly"
